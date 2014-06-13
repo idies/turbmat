9842dfe7eb540ae2eae9fd08bb8e4dac4a33bae9
kalin <kalin@743f73a2-0892-4f1a-8574-ff7a07c3af4f>
2014-06-13 17:04:47 +0000

    Adding the mixing dataset to the Web pages and Web-services (the reference to production site needs to be updated for everything to work and build correctly).
    Adding thresholding functionality.
    
    git-svn-id: svn+ssh://pha/home/SVN/trunk/turbulence/turbmat@10846 743f73a2-0892-4f1a-8574-ff7a07c3af4f

---
+++ @TurbulenceService/GetDensityGradient.m
@@ -0,0 +1,83 @@
+%
+% Turbmat - a Matlab library for the JHU Turbulence Database Cluster
+%   
+% Get*.m, part of Turbmat
+%
+
+%
+% This file is part of Turbmat.
+% 
+% Turbmat is free software: you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free
+% Software Foundation, either version 3 of the License, or (at your option)
+% any later version.
+% 
+% Turbmat is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+% more details.
+% 
+% You should have received a copy of the GNU General Public License along
+% with Turbmat.  If not, see <http://www.gnu.org/licenses/>.
+%
+
+
+function GetDensityGradientResult = GetDensityGradient(obj,authToken,dataset,time,spatialInterpolation,temporalInterpolation,points)
+%GetDensityGradient(obj,authToken,dataset,time,spatialInterpolation,temporalInterpolation,points)
+%
+%   Retrieve the Density gradient at a fixed location
+%   
+%     Input:
+%       authToken = (string)
+%       dataset = (string)
+%       time = (float)
+%       spatialInterpolation = (SpatialInterpolation)
+%       temporalInterpolation = (TemporalInterpolation)
+%       points = (ArrayOfPoint3)
+%   
+%     Output:
+%       GetDensityGradientResult = (ArrayOfVector3)
+
+% Build up the argument lists.
+data.val.authToken.name = 'authToken';
+data.val.authToken.val = authToken;
+data.val.authToken.type = '{http://www.w3.org/2001/XMLSchema}string';
+data.val.dataset.name = 'dataset';
+data.val.dataset.val = dataset;
+data.val.dataset.type = '{http://www.w3.org/2001/XMLSchema}string';
+data.val.time.name = 'time';
+data.val.time.val = time;
+data.val.time.type = '{http://www.w3.org/2001/XMLSchema}float';
+data.val.spatialInterpolation.name = 'spatialInterpolation';
+data.val.spatialInterpolation.val = spatialInterpolation;
+data.val.spatialInterpolation.type = '{http://turbulence.pha.jhu.edu/}SpatialInterpolation';
+data.val.temporalInterpolation.name = 'temporalInterpolation';
+data.val.temporalInterpolation.val = temporalInterpolation;
+data.val.temporalInterpolation.type = '{http://turbulence.pha.jhu.edu/}TemporalInterpolation';
+data.val.points.name = 'points';
+data.val.points.wrap = 'Point3';
+data.val.points.parallel = 1;
+data.val.points.val.x.name = 'x';
+data.val.points.val.x.val = points(1,:);
+data.val.points.val.y.name = 'y';
+data.val.points.val.y.val = points(2,:);
+data.val.points.val.z.name = 'z';
+data.val.points.val.z.val = points(3,:);
+
+% Create the message, make the call, and convert the response into a variable.
+soapMessage = createSoapMessage( ...
+    'http://turbulence.pha.jhu.edu/', ...
+    'GetDensityGradient', ...
+    data,'document');
+response = callSoapService( ...
+    obj.endpoint, ...
+    'http://turbulence.pha.jhu.edu/GetDensityGradient', ...
+    soapMessage);
+GetDensityGradientResult = parseSoapResponse(response);
+
+% Fault message handling
+if isfield(GetDensityGradientResult, 'faultstring')
+    error('faultcode: %s\nfaultstring: %s\n', ...
+        GetDensityGradientResult.faultcode, ...
+        GetDensityGradientResult.faultstring);
+end
---
+++ mixingm.m
@@ -0,0 +1,249 @@
+%
+% Turbmat - a Matlab library for the JHU Turbulence Database Cluster
+%   
+% Sample code, part of Turbmat
+%
+
+%
+% Written by:
+%  
+% Jason Graham
+% The Johns Hopkins University
+% Department of Mechanical Engineering
+% jgraha8@gmail.com
+%
+
+%
+% Modified by:
+% 
+% Kalin Kanov 
+% The Johns Hopkins University
+% Department of Computer Science
+% kalin@cs.jhu.edu
+%
+
+%
+% This file is part of Turbmat.
+% 
+% Turbmat is free software: you can redistribute it and/or modify it under
+% the terms of the GNU General Public License as published by the Free
+% Software Foundation, either version 3 of the License, or (at your option)
+% any later version.
+% 
+% Turbmat is distributed in the hope that it will be useful, but WITHOUT
+% ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+% more details.
+% 
+% You should have received a copy of the GNU General Public License along
+% with Turbmat.  If not, see <http://www.gnu.org/licenses/>.
+%
+
+clear all;
+close all;
+
+authkey = 'edu.jhu.pha.turbulence.testing-201311';
+dataset = 'mixing';
+
+% ---- Temporal Interpolation Options ----
+NoTInt   = 'None' ; % No temporal interpolation
+PCHIPInt = 'PCHIP'; % Piecewise cubic Hermit interpolation in time
+
+% ---- Spatial Interpolation Flags for getVelocity &amp; getVelocityAndPressure ----
+NoSInt = 'None'; % No spatial interpolation
+Lag4   = 'Lag4'; % 4th order Lagrangian interpolation in space
+Lag6   = 'Lag6'; % 6th order Lagrangian interpolation in space
+Lag8   = 'Lag8'; % 8th order Lagrangian interpolation in space
+
+% ---- Spatial Differentiation &amp; Interpolation Flags for getVelocityGradient &amp; getPressureGradient ----
+FD4NoInt = 'None_Fd4' ; % 4th order finite differential scheme for grid values, no spatial interpolation
+FD6NoInt = 'None_Fd6' ; % 6th order finite differential scheme for grid values, no spatial interpolation
+FD8NoInt = 'None_Fd8' ; % 8th order finite differential scheme for grid values, no spatial interpolation
+FD4Lag4  = 'Fd4Lag4'  ; % 4th order finite differential scheme for grid values, 4th order Lagrangian interpolation in space
+
+%  Set time step to sample
+time = 5.0;
+
+% getPosition integration settings
+startTime=5.0;
+endTime=5.08;
+lagDt=0.02; 
+
+npoints = 10;
+
+% for box filtering
+field = 'velocity';
+dx = 2. * pi / 1024;
+filterwidth = 7. * dx;
+spacing = 4. * dx;
+
+points = zeros(3,npoints);
+result1  = zeros(npoints);
+result3  = zeros(3,npoints);
+result4  = zeros(4,npoints);
+result6  = zeros(6,npoints);
+result9  = zeros(9,npoints);
+result18 = zeros(18,npoints);
+
+%  Set spatial locations to sample
+for p = 1:npoints
+  points(1,p) = 0.20 * p;
+  points(2,p) = 0.50 * p;
+  points(3,p) = 0.15 * p;
+end
+
+fprintf('\nCoordinates of 10 points where variables are requested:\n');
+for p = 1:npoints
+    fprintf(1,'%3i: %13.6e, %13.6e, %13.6e\n', p, points(1,p),  points(2,p),  points(3,p));
+end
+
+fprintf('\nRequesting velocity at %i points...\n',npoints);
+result3 =  getVelocity (authkey, dataset, time, Lag6, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: %13.6e, %13.6e, %13.6e\n', p, result3(1,p),  result3(2,p),  result3(3,p));
+end
+
+fprintf('\nRequesting velocity and pressure at %i points...\n',npoints);
+result4 = getVelocityAndPressure (authkey, dataset, time, Lag6, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: %13.6e, %13.6e, %13.6e, %13.6e\n', p, result4(1,p),  result4(2,p),  result4(3,p), result4(4,p));
+end
+
+fprintf('\nRequesting velocity gradient at %i points...\n',npoints);
+result9 = getVelocityGradient (authkey, dataset, time,  FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: duxdx=%13.6e, duxdy=%13.6e, duxdz=%13.6e, ', p, result9(1,p), result9(2,p), result9(3,p));
+  fprintf(1,'duydx=%13.6e, duydy=%13.6e, duydz=%13.6e, ', result9(4,p), result9(5,p), result9(6,p));
+  fprintf(1,'duzdx=%13.6e, duzdy=%13.6e, duzdz=%13.6e\n', result9(7,p), result9(8,p), result9(9,p));
+end
+
+fprintf('\nRequesting velocity hessian at %i points...\n',npoints);
+result18 = getVelocityHessian (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: d2uxdxdx=%13.6e, d2uxdxdy=%13.6e, d2uxdxdz=%13.6e, ', p, result18(1,p), result18(2,p), result18(3,p));
+  fprintf(1,'d2uxdydy=%13.6e, d2uxdydz=%13.6e, d2uxdzdz=%13.6e, ', result18(4,p), result18(5,p), result18(6,p));
+  fprintf(1,'d2uydxdx=%13.6e, d2uydxdy=%13.6e, d2uydxdz=%13.6e, ', result18(7,p), result18(8,p), result18(9,p));
+  fprintf(1,'d2uydydy=%13.6e, d2uydydz=%13.6e, d2uydzdz=%13.6e, ', result18(10,p), result18(11,p), result18(12,p));
+  fprintf(1,'d2uzdxdx=%13.6e, d2uzdxdy=%13.6e, d2uzdxdz=%13.6e, ', result18(13,p), result18(14,p), result18(15,p));
+  fprintf(1,'d2uzdydy=%13.6e, d2uzdydz=%13.6e, d2uzdzdz=%13.6e\n', result18(16,p), result18(17,p), result18(18,p));
+end
+
+fprintf('\nRequesting velocity laplacian at %i points...\n',npoints);
+result3 =  getVelocityLaplacian (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: grad2ux=%13.6e, grad2uy=%13.6e, grad2uz=%13.6e\n', p, result3(1,p),  result3(2,p),  result3(3,p));
+end
+
+fprintf('\nRequesting pressure at %i points...\n',npoints);
+result1 = getPressure (authkey, dataset, time, Lag6, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: %13.6e\n', p, result1(p));
+end
+
+fprintf('\nRequesting pressure gradient at %i points...\n',npoints);
+result3 =  getPressureGradient (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: dpdx=%13.6e, dpdy=%13.6e, dpdz=%13.6e\n', p, result3(1,p),  result3(2,p),  result3(3,p));
+end
+
+fprintf('\nRequesting pressure hessian at %i points...\n',npoints);
+result6 = getPressureHessian (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: d2pdxdx=%13.6e, d2pdxdy=%13.6e, d2pdxdz=%13.6e, ', p, result6(1,p), result6(2,p), result6(3,p));
+  fprintf(1,'d2pdydy=%13.6e, d2pdydz=%13.6e, d2pdzdz=%13.6e\n', result6(4,p), result6(5,p), result6(6,p));
+end
+
+fprintf('\nRequesting Density at %i points...\n',npoints);
+result1 = getDensity (authkey, dataset, time, Lag6, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: %13.6e\n', p, result1(p));
+end
+
+fprintf('\nRequesting Density gradient at %i points...\n',npoints);
+result3 =  getDensityGradient (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: dpdx=%13.6e, dpdy=%13.6e, dpdz=%13.6e\n', p, result3(1,p),  result3(2,p),  result3(3,p));
+end
+
+fprintf('\nRequesting Density hessian at %i points...\n',npoints);
+result6 = getDensityHessian (authkey, dataset, time, FD4Lag4, NoTInt, npoints, points);
+for p = 1:npoints
+  fprintf(1,'%3i: d2pdxdx=%13.6e, d2pdxdy=%13.6e, d2pdxdz=%13.6e, ', p, result6(1,p), result6(2,p), result6(3,p));
+  fprintf(1,'d2pdydy=%13.6e, d2pdydz=%13.6e, d2pdzdz=%13.6e\n', result6(4,p), result6(5,p), result6(6,p));
+end
+
+fprintf('\nRequesting position at %i points, starting at time %f and ending at time %f...\n',npoints,startTime,endTime);
+result3 = getPosition(authkey, dataset, startTime, endTime, lagDt, Lag6, 10, points);
+fprintf('\nCoordinates of 10 points at startTime:\n');
+for p = 1:npoints
+    fprintf(1,'%3i: %13.6e, %13.6e, %13.6e\n', p, points(1,p),  points(2,p),  points(3,p));
+end
+fprintf('\nCoordinates of 10 points at endTime:\n');
+for p = 1:npoints
+  fprintf(1,'%3i: %13.6e, %13.6e, %13.6e\n', p, result3(1,p),  result3(2,p),  result3(3,p));
+end
+
+fprintf('\nRequesting box filter of velocity at %i points\n', npoints);
+result3 = getBoxFilter(authkey, dataset, field, time, filterwidth, npoints, points);
+for p = 1:npoints
+    fprintf(1,'%3i: Vx=%13.6e, Vy=%13.6e, Vz=%13.6e\n', p, result3(1,p), result3(2,p), result3(3,p));
+end
+
+fprintf('\nRequesting SGS velocity tensor at %i points\n', npoints);
+result6 = getBoxFilterSGS(authkey, dataset, field, time, filterwidth, npoints, points);
+for p = 1:npoints
+    fprintf(1,'%3i: xx=%13.6e, yy=%13.6e, zz=%13.6e, ', p, result6(1,p), result6(2,p), result6(3,p));
+    fprintf(1,'xy=%13.6e, xz=%13.6e, yz=%13.6e\n', result6(4,p), result6(5,p), result6(6,p));
+end
+
+fprintf('\nRequesting box filter of velocity gradient tensor at %i points\n', npoints);
+result9 = getBoxFilterGradient(authkey, dataset, field, time, filterwidth, spacing, npoints, points);
+for p = 1:npoints
+    fprintf(1,'%3i: duxdx=%13.6e, duxdy=%13.6e, duxdz=%13.6e, ', p, result9(1,p), result9(2,p), result9(3,p));
+    fprintf(1,'duydx=%13.6e, duydy=%13.6e, duydz=%13.6e, ', result9(4,p), result9(5,p), result9(6,p));
+    fprintf(1,'duzdx=%13.6e, duzdy=%13.6e, duzdz=%13.6e\n', result9(7,p), result9(8,p), result9(9,p));
+end
+
+% ///////////////////////////////////////////////////////////
+% ////////////// GENERATE A SIMPLE CONTOUR PLOT /////////////
+%////////////////////////////////////////////////////////////
+
+%  Chose a random time step
+time = 10.0;
+spacing = 2 * 2.0*pi/1024;
+
+% Set domain size and random position
+nx = 64;
+ny = nx;
+xoff = 2*pi*rand; 
+yoff = 2*pi*rand;
+zoff = 2*pi*rand;
+npoints = nx*ny;
+
+clear points;
+
+% Create surface
+x = linspace(0, (nx-1)*spacing, nx) + xoff;
+y = linspace(0, (ny-1)*spacing, ny) + yoff;
+[X Y] = meshgrid(x, y);
+points(1:2,:) = [X(:)'; Y(:)'];
+points(3,:) = zoff;
+    
+% Get the velocity at each point
+%fprintf('\nRequesting velocity at %i points...\n',npoints);
+fprintf('\nRequesting density at (%ix%i) points for velocity contour plot...\n',nx,ny);
+result1 = getDensity(authkey, dataset, time, Lag4, NoTInt, npoints, points);
+
+% Calculate velocity magnitude
+z = result1(1,:);
+Z = transpose(reshape(z, nx, ny));
+
+% Plot velocity magnitude contours
+contourf(X, Y, Z, 30, 'LineStyle', 'none');
+set(gca, 'FontSize', 11)
+title('Density', 'FontSize', 13, 'FontWeight', 'bold');
+xlabel('x', 'FontSize', 12, 'FontWeight', 'bold');
+ylabel('y', 'FontSize', 12, 'FontWeight', 'bold');
+colorbar('FontSize', 12);
+axis([xoff max(x) yoff max(y)]);
+set(gca, 'TickDir', 'out', 'TickLength', [.02 .02],'XMinorTick', 'on', 'YMinorTick', 'on');
